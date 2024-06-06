792f1b7ec58e4a0ea661d57e8987ab885097c43d
salaudeen <olayiwolasalaudeen@gmail.com>
2024-06-06 14:55:00 +0100

    adding the creat order molder the button insaide third work

--- src/Component/ShowOrder.jsx
+++ src/Component/ShowOrder.jsx
@@ -47,7 +47,7 @@ const ShowOrder = () => {
               <label htmlFor="pickAddress">
                 <small>
                   Kindly give detailed address, please select from the available
-                  dropdown landmark option that'll appear as you type
+                  dropdown landmark option that will appear as you type
                 </small>
               </label>
             </div>
@@ -80,7 +80,9 @@ const ShowOrder = () => {
                   name="deliveryMode"
                   value="bicycle"
                 />
-                <label htmlFor="Bicycle"> Bicycle </label>
+                <label htmlFor="Bicycle">
+                  <small> Bicycle </small>{" "}
+                </label>
               </div>
               <div>
                 <input
@@ -89,11 +91,17 @@ const ShowOrder = () => {
                   name="deliveryMode"
                   value="Bike"
                 />
-                <label htmlFor="Bike"> Bike </label>
+                <label htmlFor="Bike">
+                  {" "}
+                  <small> Bike </small>{" "}
+                </label>
               </div>
               <div>
                 <input type="radio" id="Car" name="deliveryMode" value="car" />
-                <label htmlFor="Car"> Car </label>
+                <label htmlFor="Car">
+                  {" "}
+                  <small> Car </small>{" "}
+                </label>
               </div>
               <div>
                 <input
@@ -102,11 +110,90 @@ const ShowOrder = () => {
                   name="deliveryMode"
                   value="truck"
                 />
-                <label htmlFor="Truck"> Truck </label>
+                <label htmlFor="Truck">
+                  <small> Truck </small>{" "}
+                </label>
+              </div>
+            </div>
+            <div>
+              <textarea
+                name=""
+                id=""
+                className=" rounded w-full h-20 px-5"
+                placeholder="note"
+              ></textarea>
+            </div>
+            <div></div>
+            <TextInput
+              id="receiverName"
+              placeholder="Drop off full name"
+              required
+            />
+            <label htmlFor="receiverName">
+              <small>Receiver name</small>
+            </label>
+            <TextInput
+              id="receiverName"
+              placeholder="Drop off Address"
+              required
+            />
+            <label htmlFor="receiverName">
+              <small>
+                Receiver address, please select from the available dropdown
+                landmark option that will appear as you type
+              </small>
+            </label>
+            <TextInput
+              id="receiverName"
+              placeholder="Drop off phone number"
+              required
+            />
+            <label htmlFor="receiverName">
+              <small>Kindly provide main line and other line</small>
+            </label>
+            <div>
+              <small>Delivery Type</small>
+              <div>
+                <input
+                  type="radio"
+                  id="sameCity"
+                  name="deliveryType"
+                  value="sameCity"
+                  title="sameCity"
+                />
+                <label htmlFor="SameCity">
+                  <small> City (delivery within same state or city) </small>
+                </label>
+
+                <div>
+                  <input
+                    type="radio"
+                    id="interState"
+                    name="deliveryType"
+                    value="interState"
+                    title="interState"
+                  />
+                  <label htmlFor="interState">
+                    <small> Inter-State </small>
+                  </label>
+                </div>
+
+                <div>
+                  <input
+                    type="radio"
+                    id="interCountry"
+                    name="deliveryType"
+                    value="interCountry"
+                    title="interCountry"
+                  />
+                  <label htmlFor="interCountry">
+                    <small> Inter-Country </small>
+                  </label>
+                </div>
               </div>
             </div>
             <div className=" flex justify-end">
-              <Button className=" bg-green-800 ">Add </Button>
+              <Button className=" bg-green-800 "><small>Add</small></Button>
             </div>
             <div className=" flex justify-center">
               <Button className=" bg-green-800 py-1 px-6">Preview </Button>
